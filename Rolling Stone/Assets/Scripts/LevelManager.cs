3156613424744fff66c678c979ec0043791a39f9
Brenden <brenden.e.barton@gmail.com>
2017-12-03 00:17:02 -0500

    Day 1
    Day 1

---
+++ Rolling Stone/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public void LoadLevel(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void QuitGame()
+    {
+        print("Exiting Application.");
+        Application.Quit();
+    }
+}
---
+++ Rolling Stone/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float thrust;
+    public Rigidbody rb;
+    public Transform start;
+    public Transform player;
+    Vector3 startScale;
+    float startDistance;
+
+    void Start()
+    {
+        startDistance = Vector3.Distance(start.transform.position, transform.position);
+        startScale = transform.localScale;
+    }
+
+    private void Update()
+    {
+        float curDistance = Vector3.Distance(start.transform.position, transform.position) - startDistance;
+        player.localScale = (startScale * curDistance) / 10;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            print("GOING RIGHT!");
+            rb.AddRelativeForce(transform.right * thrust);
+        }
+
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            print("GOING LEFT!");
+            rb.AddRelativeForce((transform.right * -1) * thrust);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            print("FULL SPEED!");
+            rb.AddRelativeForce(transform.forward * thrust);
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            print("HOP BACKWARD!");
+            rb.AddRelativeForce((transform.forward * -1) * thrust);
+        }
+    } // Controls here.
+}

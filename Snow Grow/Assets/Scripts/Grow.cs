06b7b2334e373d0fc671fa5dea4092e3a3d8ece5
Brenden <brenden.e.barton@gmail.com>
2017-12-04 17:40:59 -0500

    Imported tree assets
    I Imported the models for some trees and rocks to decorate the terrain with.

--- Snow Grow/Assets/Scripts/Grow.cs
+++ Snow Grow/Assets/Scripts/Grow.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Grow : MonoBehaviour
 {
-
+    public Text text;                       // Reference to "Score" Textbox.
     public Transform player;                // Reference to player's transform data.
 
     private bool isGrounded = false;
     public Vector3 currentScale;            // The player's current scale.
-    public float scaleRate = 0.02f;          // The rate at which to increase size.
+    public float scaleRate = 0.02f;         // The rate at which to increase size.
+
+    int score;                              // Holds the player's score, and sends it to the "Score" Textbox. 
 
 
     void LateUpdate()
@@ -17,6 +20,8 @@ public class Grow : MonoBehaviour
         if (isGrounded == true)
         {
             player.localScale += new Vector3(scaleRate, scaleRate, scaleRate);
+            score = (int)player.localScale.x * 10;
+            text.text = score.ToString();
         }
     }
 
